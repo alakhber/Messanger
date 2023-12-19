b80d45e11498cbe6d5eadee298363f0cbf958942
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-12-19 18:28:07 +0400

    first commit

---
+++ app/Http/Controllers/TelegramMessanger.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TelegramMessanger extends Controller
+{
+    public function handler(Request $request){
+        
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::match(['GET', 'POST'], '/', function (Request $request) {
+    $telegramBotToken = config('app.tg_bot');
+
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    Log::alert($update);
+    if (isset($update['message'])) {
+        $message = $update['message'];
+        $chat_id = $message['chat']['id'];
+        $text = $message['text'];
+        if ($text == '/start') {
+            $response = 'Merhaba! Botunuz çalışıyor.';
+            file_get_contents("https://api.telegram.org/bot$telegramBotToken/sendMessage?chat_id=$chat_id&text=$response");
+        }
+    }
+});
