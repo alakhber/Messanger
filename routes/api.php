cc85f9416b96f289dae7b7bc504ceb6697b93703
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-12-19 18:29:18 +0400

    test

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::match(['GET', 'POST'], '/', function (Request $request) {
     $telegramBotToken = config('app.tg_bot');
